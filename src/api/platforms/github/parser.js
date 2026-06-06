f48a2bb90375385b612970b511ae9c812051feef
Liad Hossain <liadhossain@gmail.com>
2026-06-06 11:22:23 +0600

    feat: added llm to generate summary for pull requests

--- src/api/platforms/github/parser.js
+++ src/api/platforms/github/parser.js
@@ -2,16 +2,16 @@ import { PullRequestOpenedSchema } from './schema';
 import { ActivityTypes, ActivitySubTypes } from './constants';
 
 export function parseGitHubWebhookPayload(eventType, rawPayload) {
-  let activity = null;
+  let document = null;
   try {
     if (
       eventType === ActivityTypes.PULL_REQUEST &&
       rawPayload?.action === ActivitySubTypes.PR_OPENED
     ) {
-      activity = PullRequestOpenedSchema.extract(rawPayload);
+      document = PullRequestOpenedSchema.extract(rawPayload);
     }
   } catch (e) {
     console.log('Error in parseGitHubWebhookPayload:', e);
   }
-  return activity;
+  return document;
 }
